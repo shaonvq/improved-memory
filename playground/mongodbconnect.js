24ac743065d39a309f8b02aa21b200545647adbe
shaonvQ <shaonvq@gmail.com>
2018-03-03 00:43:03 -0800

    idk

--- playground/mongodbconnect.js
+++ playground/mongodbconnect.js
@@ -4,7 +4,7 @@ MongoClient.connect('mongodb://localhost:27017', (err, client) => {
     var db = client.db('Todos');    
     console.log('connected to mongodb server');
 
-    db.collection('apple').insertOne({
+    db.collection('Todos').insertOne({
         text:'Something to do.',
         completed:false
     },(err,result)=>{
